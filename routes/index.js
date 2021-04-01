bcfd8234d6f295dcc2e1a7062fafc40570b4b805
phungkimhanh <phungkimhanh2k1@gmail.com>
2021-04-02 01:12:16 +0700

    app commit

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/sign_in', function(req, res, next) {
+  res.render('sign_in');
+});
+
+router.get('/sign_up', function(req, res, next) {
+  res.render('sign_up');
+});
+router.get('/profile', function(req, res, next) {
+  res.render('profile',{layout: 'main_home'});
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('main_home');
+});
+
+router.get('/messenger', function(req, res, next) {
+  res.render('messenger',{layout: 'main_home'});
+});
 module.exports = router;
