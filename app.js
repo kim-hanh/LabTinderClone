d3b9fd899f4d13046d4c61ccd30954c59d1c1226
phungkimhanh <phungkimhanh2k1@gmail.com>
2021-04-18 22:49:06 +0700

    app commit

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var hbs = require('hbs')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+hbs.registerHelper('checked', function (value, test) {
+  if (value == undefined) return '';
+  return value == test ? 'checked' : '';
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -18,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'assets')));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/uploads/photos')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
